a9e1a119055ef8456ae4fe152bb011c217ecfe7a
sebastianfloresxp <sebastian.s.floress@gmail.com>
2026-04-06 11:32:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -167,7 +167,7 @@
 
   function buildFunctions() {
     const list = [
-      directFunction("log(log(x+3))", "analytic", (x) => 0.35 + 0.45 * 10000000000*x),
+      directFunction("log(log(x+3))", "analytic", (x) => 0.35 + 0.45 * Math.pow(x,(10*x))),
       directFunction("log(x+1)", "analytic", (x) => 0.8 + 0.8 * Math.log(x + 1.5)),
       directFunction("sqrt(log(x+1))", "analytic", (x) => 1.1 + 0.95 * Math.sqrt(Math.log(x + 1.6))),
       directFunction("log(x+1)^2", "analytic", (x) => 1.4 + 0.62 * Math.pow(Math.log(x + 1.8), 2)),
